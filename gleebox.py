2489d74bf2b5f0571b8b77ad8aa66dd61c658678
Tom Wu <project.w@gmail.com>
2012-11-11 02:22:23 +0000

    update git push

--- gleebox.py
+++ gleebox.py
@@ -8,7 +8,10 @@ def hello_world(request):
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('hello', '/hello/{name}')
+    config.add_route('hello', '/')
+    config.include('akhet')
+    # Put your regular routes here.
+    config.add_static_route('zzz', 'static', cache_max_age=3600)
     config.add_view(hello_world, route_name='hello')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
