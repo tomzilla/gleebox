493707d4ac2a0d3f74fe4a4c1587e1bbb9be573d
Tom Wu <project.w@gmail.com>
2012-11-12 10:45:22 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ DEPLOY_DIR=/var/www-staging/gleebox
 rsync -qrlpgoDz -c --delete $EXCLUDES --progress $BASEDIR $DEPLOY_DIR
 cd $DEPLOY_DIR
 pwd
+ls
 ./runpaste-staging stop
 ./runpaste-staging start
 
