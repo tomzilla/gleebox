af70b77eb6d8576b8a5148264585a2d1115b70ec
Tom Wu <project.w@gmail.com>
2012-11-12 09:51:13 +0000

    combine

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
 BASEDIR=$(dirname $0)
+DEPLOY_DIR=/var/www-staging/
 
-rsync -qrlpgoDz -c --delete $EXCLUDES --progress $BASEDIR/../gleebox /var/www-staging/
+rsync -qrlpgoDz -c --delete $EXCLUDES --progress $BASEDIR/../gleebox $DEPLOY_DIR
+cd $DEPLOY_DIR/gleebox
+./runpaste-staging restart
+
